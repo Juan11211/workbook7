6ab74649c6427b71fb723d4e1bc4c24f116b9a22
Juan11211 <avilesjuan628@gmail.com>
2023-12-03 16:54:05 -0500

    post method working

--- JsonPlaceHolder/todo/scripts/addTodo.js
+++ JsonPlaceHolder/todo/scripts/addTodo.js
@@ -2,30 +2,49 @@
 
 window.onload = () => {
     let addButton = document.getElementById('addButton'); 
-    let displayTodo = document.getElementById('displayTodo')
+    let displayTodoPost = document.getElementById('displayTodo')
 
-    addButton.onclick = addTodoApi; 
+    addButton.onclick = postTodoApi; 
 }
 
-function addTodoApi(e){
+function postTodoApi(e){
     e.preventDefault(); 
 
-    const userId = document.getElementById('userId').value;
-    const title = document.getElementById('userId').value
-    const completed = document.getElementById('userId')
+    const userId = document.getElementById('userIdInput').value;
+    const title = document.getElementById('titleInput').value;
+    const completed = document.getElementById('completed').value;
+    
+    const bodyData = JSON.stringify({
+        userId: userId,
+        title: title,
+        completed: completed
+    })
 
 
-    fetch('`https://jsonplaceholder.typicode.com/todos/', {
+    fetch('https://jsonplaceholder.typicode.com/todos/', {
         method: "POST",
         body: bodyData, 
         headers: {"Content-type":   
-            "application/json; charset=UTF-8"}
+            "application/json"}
     })
     .then(res => res.json())
-    .then(json => {
-        console.log(json)
-    })
+    .then(todo => {
+        console.log(todo);
+        displayPost(todo);
+    })    
     .catch(err => {
-        console.error(`this is not posting`)
+        console.error(`this is not posting`, err)
     });
+}
+
+
+function displayPost(todo) {
+    let displayTodoPost = document.getElementById('displayTodo');
+
+    displayTodoPost.innerHTML += `
+        <div class="container">
+            <h3>Title: ${todo.title}</h3>
+            <p>Completed: ${todo.completed}</p>
+        </div>
+    `;
 }
